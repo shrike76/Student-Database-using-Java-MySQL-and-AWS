838b1cc61d203766b00973b338784bacf7e26ec6
Colton Weber <shrike76@aim.com>
2020-04-11 16:06:47 -0500

    Basic table creation.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
-// This Assignment is Submitted by Gandhimathi
+
+import java.sql.*;
+
+// This Assignment is Submitted by Colton Weber
 public class Controller {
+    public void initialize() {
+        try{
+            final String DB_URL = "jdbc:derby://localhost:1527/LibraryDB;";
+            Connection conn = DriverManager.getConnection(DB_URL);
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("create table LIBRARY\n" +
+                    "(\n" +
+                    "    ISBN   INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),\n" +
+                    "    TITLE_OF_MEDIA  VARCHAR(99),\n" +
+                    "    AUTHOR          VARCHAR(40),\n" +
+                    "    LOCATION        VARCHAR(20),\n" +
+                    "    QUANTITY        INTEGER\n" +
+                    ")");
+            stmt.execute("alter table LIBRARY\n" +
+                    "    add constraint LIBRARY_PK\n" +
+                    "        primary key (ISBN)\n");
+            stmt.close();
+            System.out.println("Success!..Table created");
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Table already exists");
+        }
+    }
 }
